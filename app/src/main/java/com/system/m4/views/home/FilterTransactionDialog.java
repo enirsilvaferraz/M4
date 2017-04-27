9630b00505983b8f67eaa5b4ee360912348fb304
Enir Francisco Silva Ferraz <eferraz@ciandt.com>
2017-04-27 15:15:10 -0300

    [IMPL] Nova home - filtro

---
+++ app/src/main/java/com/system/m4/views/home/FilterTransactionDialog.java
@@ -0,0 +1,12 @@
+package com.system.m4.views.home;
+
+import com.system.m4.views.BaseDialogFragment;
+
+/**
+ * Created by eferraz on 27/04/17.
+ */
+
+public class FilterTransactionDialog extends BaseDialogFragment {
+
+
+}
