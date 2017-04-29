a7f4781f0db63e77f3afa3439d203875aea9c650
Enir Francisco Silva Ferraz <eferraz@ciandt.com>
2017-04-29 13:00:43 -0300

    [IMPL] Nova home - filtro

--- app/src/main/java/com/system/m4/views/home/FilterTransactionDialog.java
+++ app/src/main/java/com/system/m4/views/home/FilterTransactionDialog.java
@@ -1,5 +1,13 @@
 package com.system.m4.views.home;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.system.m4.R;
 import com.system.m4.views.BaseDialogFragment;
 
 /**
@@ -9,4 +17,25 @@ import com.system.m4.views.BaseDialogFragment;
 public class FilterTransactionDialog extends BaseDialogFragment {
 
 
+    public static DialogFragment newInstance() {
+
+        Bundle bundle = new Bundle();
+
+        FilterTransactionDialog fragment = new FilterTransactionDialog();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.dialog_transaction_filter, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setTitle(R.string.transaction_filter_title);
+    }
 }
